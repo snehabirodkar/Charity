6cb7e103f4a6fdf4e3da1cfea3966c8522762ec6
SnehaBirodkar <snehabirodkar31@gmail.com>
2020-05-06 21:35:16 +0530

    counter section completed

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,3 +3,18 @@ if(module.hot){
     module.hot.accept();
 }
 console.log("changes!!");
+
+
+$('.action').owlCarousel({
+  loop:true,
+  margin:30,
+// autoplay:true,
+  navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+  nav:false,
+  dots:false,
+// autoplayHoverPause: true,
+// autoplaySpeed: 800,
+  responsive:{
+     
+  }
+});
